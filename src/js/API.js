f37366e3391e96f757e20ccf4c00ad6b2bf6d29c
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-02-06 22:53:50 +0100

    Added API response, basic layout

--- src/js/API.js
+++ src/js/API.js
@@ -2,23 +2,21 @@ const searchBar = document.querySelector(".search-input");
 const names = document.querySelectorAll(".result-info");
 const fetchButton = document.querySelector("[data-fetch]");
 
-
-
-const loadInfo = (req) => {
+const loadInfo = (user_api, repo_api) => {
   const avatar = document.querySelector("[data-avatar]");
   const username = document.querySelector("[data-username]");
   const userURL = document.querySelector("[data-url]");
   const creationDate = document.querySelector("[data-creation-date]");
   const dataLocation = document.querySelector("[data-location]");
 
+  //loads information about user such as avatar, username, github URL
   const userInformation = () => {
-    fetch(req)
+    fetch(user_api)
       .then((response) => {
-          const processing = response.json();
+        const processing = response.json();
         return processing;
       })
-        .then((processed) => {
-           
+      .then((processed) => {
         avatar.src = processed.avatar_url;
         username.textContent = processed.name;
         userURL.textContent = processed.html_url;
@@ -30,37 +28,45 @@ const loadInfo = (req) => {
         console.log(processed);
       });
   };
-    userInformation();
-   
-}
+  //loads infomartion about user's repositories
+  const userRipos = () => {
+    fetch(repo_api)
+      .then((response) => {
+        const jsonResponse = response.json();
+        return jsonResponse;
+      })
+      .then((processed) => {
+        const repoParent = document.querySelector(".repo"); //parent where elements will be created
+        //if p
+        while (repoParent.firstChild) {
+          repoParent.firstChild.remove();
+        }
+        
+        //creates elements that equal a number of repositories of a user
+        //elements contain name of a repository
+        for (var i = 0; i < processed.length; i++) {
+          const repoChild = document.createElement("div");
+          repoChild.classList.add("repo-child");
+          repoChild.textContent = [processed[i].name];
+          repoParent.appendChild(repoChild);
+        }
+      });
+  };
 
+  userInformation();
+  userRipos();
+};
+
+
+//on clicking the search button it checks the input value and later adds it to API as a string
+//to later retrieve information about a user
 fetchButton.addEventListener("click", () => {
-    const searchedUser = document.querySelector('[data-search]');
+  const searchedUser = document.querySelector("[data-search]"); //data input
 
-    let GITHUB_USER = "https://api.github.com/users/" + searchedUser.value;
-    console.log(GITHUB_USER)
-    loadInfo(GITHUB_USER)
+  let GITHUB_USER = "https://api.github.com/users/" + searchedUser.value;
+  let GITHUB_REPO =
+    "https://api.github.com/users/" + searchedUser.value + "/repos";
+  console.log(GITHUB_USER, GITHUB_REPO);
+  loadInfo(GITHUB_USER, GITHUB_REPO);
 });
 
-//searchBar.addEventListener("input", (e) => {
-//  checkNames(e);
-//});
-
-//const checkNames = (e) => {
-//  const value = e.target.value;
-//  //const myArray = [];
-//
-//  names.forEach((e) => {
-//    // myArray.push(e.innerHTML.toLowerCase())
-//    //console.log(myArray)
-//    if (e.textContent.includes(value)) {
-//      e.classList.toggle("hide", false);
-//      return;
-//    }
-//
-//    if (!e.textContent.includes(value)) {
-//      e.classList.toggle("hide", true);
-//      return;
-//    }
-//  });
-//};
--- src/js/App.js
+++ src/js/App.js
@@ -1,32 +1,7 @@
 import "./API.js";
 
-const repoInfo = () => {
-  var temp = document.querySelector(".test");
-  var clone = temp.content.cloneNode(true);
-  document.querySelector(".repo").appendChild(clone);
-  console.log(clone);
-};
-  fetch("https://api.github.com/users/octocat/repos")
-    .then((response) => {
-      const jsonResponse = response.json();
-      return jsonResponse;
-    })
-    .then((processed) => {
-      console.log(processed[0].name);
-        for (let i = 0; i < processed.length; i++) {
-          
-        console.log(processed[i].name);
-      }
-    }); 
 
 
-const testFunc = () => {
-        
-    }
-addEventListener("load", () => {
 
 
-  for (let i = 0; i < 10; i++) {
-    repoInfo();
-  }
-});
+
