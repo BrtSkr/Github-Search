d27b70e8c5833228cacb0a4672e45aeda84a0dbf
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-02-09 07:21:26 +0100

    Update

--- dist/main.d9ee62f6.js
+++ dist/main.d9ee62f6.js
@@ -222,7 +222,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< Updated upstream
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "45451" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35177" + '/');
+>>>>>>> Stashed changes
 
   ws.onmessage = function (event) {
     checkedAssets = {};
